4ef7729c29036867c8e0cb28798f49d5abc64626
Suhel Mehta <suhelmehta@outlook.com>
2020-10-02 23:36:27 +0530

    Deploy the app with firebase
    Added trailer popup
    Deploy app with firebase

--- src/Row.js
+++ src/Row.js
@@ -8,6 +8,8 @@ const base_url = "https://image.tmdb.org/t/p/w500"
 function Row({ title, fetchUrl, isLargeRow }) {
 
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+
     useEffect(() => {
        // if [], run once when the row loads, and don't run it again
        async function fetchData() {
@@ -18,6 +20,35 @@ function Row({ title, fetchUrl, isLargeRow }) {
        fetchData();
     },[fetchUrl]);
 
+  function fetchVideoLink(videoId) {
+    const url = `https://api.themoviedb.org/3/movie/${videoId}/videos?api_key=f40fbd9c588bef8f7d48ac7a01532821`;
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data.results.length !== 0)
+          console.log(res.data.results[0]?.key);
+        setTrailerUrl(res.data.results[0]?.key);
+      })
+      .then((movieKey) => movieKey);
+  }
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      fetchVideoLink(movie.id);
+    }
+  };
+
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
 
     //console.table(movies);
     return (
@@ -27,11 +58,12 @@ function Row({ title, fetchUrl, isLargeRow }) {
             {movies.map(movie => (
                 <img 
                 key={movie.id}
+                onClick={() => handleClick(movie)}
                     className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                 src={`${base_url}${isLargeRow ? movie.poster_path : (movie.backdrop_path ? movie.backdrop_path : movie.poster_path)}`} alt={movie.original_title}/>
             ))}
            </div>
-            <YouTube videoId={} opts={opts}/>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
